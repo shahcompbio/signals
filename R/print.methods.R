8ee207c6938b7eb7c1801d6b0bbf177b9ed524dd
marcjwilliams1 <marcjwilliams1@gmail.com>
2020-05-08 13:17:33 +0100

    add inferred LOH error to print statements

--- R/print.methods.R
+++ R/print.methods.R
@@ -9,7 +9,8 @@ print.hscn = function(x, ...) {
   stopifnot(is.hscn(x))
   cat("Haplotype specific copy number object \n \n")
   cat(paste0("Number of cells: ", length(unique(x$data$cell_id)), "\n"))
-  cat(paste0("Bin size : ", (x$data$end[1] - x$data$start[1] + 1) / 1e6, " Mb \n"))
+  cat(paste0("Bin size: ", (x$data$end[1] - x$data$start[1] + 1) / 1e6, " Mb \n"))
+  cat(paste0("Inferred LOH error rate: ", round(x$loherror, 3)))
 }
 
 #' @export
@@ -18,4 +19,5 @@ print.ascn = function(x, ...) {
   cat("Allele specific copy number object \n \n")
   cat(paste0("Number of cells: ", length(unique(x$data$cell_id)), "\n"))
   cat(paste0("Bin size : ", (x$data$end[1] - x$data$start[1] + 1) / 1e6, " Mb \n"))
+  cat(paste0("Inferred LOH error rate : ", round(x$loherror, 3), " Mb \n"))
 }
