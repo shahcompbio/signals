4fd5125d4d5f607996a0f3129cd37a31fcf4f37e
marcjwilliams1 <marcjwilliams1@gmail.com>
2020-05-18 11:58:05 +0100

    print methods for bb models

--- R/print.methods.R
+++ R/print.methods.R
@@ -25,4 +25,8 @@ print.ascn = function(x, ...) {
   cat(paste0("Number of cells: ", length(unique(x$data$cell_id)), "\n"))
   cat(paste0("Bin size : ", (x$data$end[1] - x$data$start[1] + 1) / 1e6, " Mb \n"))
   cat(paste0("Inferred LOH error rate : ", round(x$loherror, 3)))
+  if (x$likelihood$likelihood == "betabinomial"){
+    cat(paste0("\t Inferred over dispersion: ", round(x$likelihood$rho, 4), "\n"))
+    cat(paste0("\t Tarones Z score: ", round(x$likelihood$taronesZ, 3), "\n"))
+  }
 }
