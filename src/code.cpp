b4dd762d54b1daee802db997d1fa14fc1576a4f0
marcjwilliams1 <marcjwilliams1@gmail.com>
2020-05-30 13:59:46 +0100

    feature: rcpp implementation of viterbi

---
+++ src/code.cpp
@@ -0,0 +1,48 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+//' @export
+// [[Rcpp::export]]
+double logspace_addcpp (double logx, double logy)
+{
+  return fmax (logx, logy) + log1p (exp (-fabs (logx - logy)));
+}
+
+//' @export
+// [[Rcpp::export]]
+NumericVector myviterbicpp(NumericMatrix emission, NumericMatrix transition, NumericVector observations)
+{
+
+  emission = transpose (emission);
+  int numStates = transition.nrow();
+  int numObs = observations.length();
+
+  observations = observations - 1;
+
+  NumericVector initial  (numStates, -log(numStates));
+
+  NumericMatrix T1 (numStates, numObs);
+  NumericMatrix T2 (numStates, numObs);
+
+  T1( _ , 0) = initial + emission( _ , observations(0));
+
+  for(int j = 1; j < numObs; j++){
+    for(int i = 0; i < numStates; i++){
+      NumericVector probs = T1 ( _ , j - 1) + transition ( _ , i ) + emission (i, observations(j));
+      T1 (i , j) = max(probs);
+      T2 (i , j) = which_max(probs);
+    }
+  }
+
+  NumericVector MLP (numObs);
+
+  MLP (numObs - 1) = T2 (which_max(T1 (_ , numObs - 1)) , numObs - 1);
+
+  for(int i = numObs - 1; i > 1; --i){
+    int zm = which_max( T1 ( _, i));
+    MLP (i - 1) = T2 (zm, i);
+  }
+
+  return(MLP);
+}
+
