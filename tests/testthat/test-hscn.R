84bef7e912df8891471a0752a98382c5c6e662f3
marcjwilliams1 <marcjwilliams1@gmail.com>
2020-06-13 17:11:44 +0100

    removed dplyr call from tests

--- tests/testthat/test-hscn.R
+++ tests/testthat/test-hscn.R
@@ -15,11 +15,10 @@ f <- f / sum(f)
 
 results_df <- orderdf(results_bb$data)
 truth_df <- orderdf(sim_data_bb$ascn)
-
-joined <- full_join(results_df, truth_df, by = c("cell_id", "chr", "start", "end"))
+nbins <- length(truth_df$cell_id)
 
 test_that("Test haplotype specific copy number inference (beta-binomial)", {
-  expect_gt(sum(joined$state_AS_phased.x == joined$state_AS_phased.y, na.rm = TRUE) / dim(joined)[1], 0.99) #test accuraccy is > 99%
+  expect_gt(sum(results_df$state_AS_phased == truth_df$state_AS_phased) / nbins, 0.99) #test accuraccy is > 99%
   expect_equal(round(results_bb$loherror, 2), loherror)
   expect_gt(results_bb$likelihood$taronesZ, 5)
   expect_true(f[["0|2"]] == 0.125)
